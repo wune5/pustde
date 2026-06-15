c48ac197344cb40d61de47d4547e2a0a05fd4b7d
龙五 <cc@pyle.cc>
2026-06-15 11:00:32 +0800

    Update BootReceiver.kt

--- flutter/android/app/src/main/kotlin/com/wune5/mingremote/BootReceiver.kt
+++ flutter/android/app/src/main/kotlin/com/wune5/mingremote/BootReceiver.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.wune5.mingremote
 
 import android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
 import android.Manifest.permission.SYSTEM_ALERT_WINDOW
